e8d7f800dd20ccecd1aa112b0120d256f92ab13f
Darrin Holst <darrinholst@gmail.com>
2012-01-03 19:28:06 -0600

    make it work on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'grit'
 
 GitVersion::Engine.routes.draw do
-  root :to => proc {|env| [200, {}, [Grit::Repo.new(Rails.root + '.git').commits.first.id[0...6]]]}
+  root :to => proc {|env| [200, {}, [ENV['COMMIT_HASH'] || Grit::Repo.new(Rails.root + '.git').commits.first.id[0...7]]]}
 end
 
 Rails.application.routes.draw do
--- lib/git-version/version.rb
+++ lib/git-version/version.rb
@@ -1,3 +1,3 @@
 module GitVersion
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
